5d39cde0818ae907a4871bf53ebbd28b18da459a
Utkarsh <utkarshgunner10@gmail.com>
2023-05-29 15:05:22 +0530

    assignment setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+
+function App() {
+  
+
+  return (
+    <>
+      <Box sx={{fontSize:50 }}>
+        Hello from Vite
+        </Box>
+    </>
+  )
+}
+
+export default App
